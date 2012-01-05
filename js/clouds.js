3d54996aa57fc1d328a995346c1ccdf7dddaa137
Jordan Adams <jordan_callum@live.co.uk>
2012-01-05 13:03:24 +0000

    Fixed initial delay and tweaked speed

--- js/clouds.js
+++ js/clouds.js
@@ -4,25 +4,37 @@
   	return Math.floor(Math.random() * (to - from + 1) + from);
   }
 
-	function spawn_cloud_loop () {
-		setTimeout(function () {
-			add_cloud();
-			spawn_cloud_loop();
-		}, rand(10000, 12000));
+
+  function calc_duration(distance, speed) {
+		return distance/speed;
 	}
+
+
+	function spawn_cloud_loop () {
+		add_cloud();
+		setTimeout(function () {
+			spawn_cloud_loop();
+		}, rand(9000, 10000));
+	}
+
 
 	function add_cloud() {
 		cloud = $('<img src="images/cloud_'+rand(1,3)+'.png" class="cloud">').appendTo('#clouds');
 
+		distance = (window.screen.width+cloud.width())*1000;
+		speed = 30;
+		time = calc_duration(distance, speed);
+
 		cloud.css({
-			top: rand(10, 150),
+			top: rand(10, 250),
 			left: -275
 		});
 
 		cloud.animate({
 			left: window.screen.width+100,
-		}, 50000, 'linear');
+		}, time, 'linear');
 	}
+
 
 	function clean_up() {
 		boundary = window.screen.width+100;
@@ -42,8 +54,11 @@
 		}, 500)
 	}
 
-	add_cloud();
-	spawn_cloud_loop();
-	clean_up();
+
+
+	$(document).ready(function() {
+		spawn_cloud_loop();
+		clean_up();
+	});
 
 })(jQuery);
